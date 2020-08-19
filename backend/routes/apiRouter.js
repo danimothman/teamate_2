a1366c21592ea9afe03e6a8462623e7b6a19e7a8
danimothman <hwankook.kim9@gmail.com>
2020-08-19 14:11:03 +0900

    test3

--- backend/routes/apiRouter.js
+++ backend/routes/apiRouter.js
@@ -1,7 +1,9 @@
 var express = require('express')
 var router = express.Router()
 var User = require('../models/User')
+var jwt = require('jsonwebtoken')
 
+const secret = "mysecu"
 router.get('/api/home',function(req, res, next){
     app.get('/data' , function(req, res , next){
         res.json({
@@ -36,11 +38,39 @@ router.post('/api/register', function(req, res, next){
         }
     })
 
-    res.json({
-        message:"OK"
-    })
+    // res.json({
+    //     message:"OK"
+    // })
 
 })
 
+router.post('/api/authenticate',function(req, res, next){
+    const {email, password} = req.body
+    // console.log(req.body)
+    // res.send("Success")
+    User.findOne({email}, function(err, user){
+        if(err) {
+            console.log(err)
+            res.status(500).json({error:'Internal error please try again'})
+        } else if (!user){
+            res.status(401).json({error:'email is not existed'})
+        } else {
+            user. isCorrectPassword(password, function(err, same){
+                // console.log(same)
+                // res.send("Success")
+                if(err){
+                    console.log(err)
+                    res.status(500).json({error:'Internal error please try again'})
+                } else if(!same){
+                    res.status(401).json({error:'Incorrect password'})
+                } else {
+                    const payload = {email};
+                    const token = jwt.sign(payload, secret, { expiresIn:'1h' })
+                    console.log(token)
+                    res.cookie('token', token,{httpOnly:true} ).sendStatus(200)
+                }})
+        }
+    })
+})
 
 module.exports = router;
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const SignUp = () => {
+    const [user, setUser] = useState({
+        email:'',
+        username:'',
+        password:''
+    })
+
+    function handelInputChange(e){
+        e.preventDefault()
+        console.log(e.target)
+        const {value, name} = e.target
+        setUser({
+            ...user,
+            [name]:value
+        })
+    }
+
+    function onSubmit(e){
+        e.preventDefault()
+        console.log(user)
+    }
+
+
+    console.log(user)
+
     return (
-        <div>
-            This is SignUp Page 
-        </div>
+        <form onSubmit={onSubmit}>
+            <h1> SignUp Page  </h1>
+            <input type="email" name="email" placeholder="Enter email" value = {user.email} onChange = {handelInputChange} required />
+            <input type="text" name="username" placeholder="Enter username" value = {user.username} onChange = {handelInputChange} required />
+            <input type="password" name="password" placeholder="Enter password" value = {user.password} onChange = {handelInputChange} required />
+            <input type="submit"value="Sign Up"/>
+        </form>
     )
 }
 
